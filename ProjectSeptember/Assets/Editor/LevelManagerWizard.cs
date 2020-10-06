bcaf0b4a7bbb20b3dcc7b5d175e5243ace778f87
David Engblom <56073597+davidengblom@users.noreply.github.com>
2020-10-06 15:29:52 +0200

    Added level manager and object spawner

---
+++ ProjectSeptember/Assets/Editor/LevelManagerWizard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class LevelManagerWizard : ScriptableWizard
+{
+    private GameObject stairPathParent;
+    private GameObject floorParent;
+
+    private void Awake()
+    {
+        stairPathParent = GameObject.Find("StairWalkables");
+        floorParent = GameObject.Find("FloorPieces");
+    }
+
+    [MenuItem("Custom Tools/Level Manager")]
+    private static void CreateWizard()
+    {
+        DisplayWizard<LevelManagerWizard>("Manage level", "Remove Stair Path", "Remove Floor");
+    }
+
+    private void OnWizardCreate()
+    {
+        int childCount = stairPathParent.transform.childCount;
+        for (int i = childCount - 1; i >= 0; i--)
+        {
+            DestroyImmediate(stairPathParent.transform.GetChild(i).gameObject);
+        }
+    }
+
+    private void OnWizardOtherButton()
+    {
+        int childCount = floorParent.transform.childCount;
+        for (int i = childCount - 1; i >= 0; i--)
+        {
+            DestroyImmediate(floorParent.transform.GetChild(i).gameObject);
+        }
+    }
+}
